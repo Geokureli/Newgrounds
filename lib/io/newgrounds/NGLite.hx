dfd6f9bb8125c765f3c3ce24dad84bd9f72b335a
George FunBook <gkurelic@gmail.com>
2018-02-16 15:11:51 -0600

    fix queue log

--- lib/io/newgrounds/NGLite.hx
+++ lib/io/newgrounds/NGLite.hx
@@ -100,7 +100,7 @@ class NGLite {
 	@:generic
 	function queueCall<T:ResultBase>(call:Call<T>):Void {
 		
-		logVerbose("queued - ${call.component}");
+		logVerbose('queued - ${call.component}');
 		
 		_queuedCalls.push(call);
 		checkQueue();
