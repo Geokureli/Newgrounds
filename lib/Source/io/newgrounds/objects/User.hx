2a3b82ed03457ca3aeca66a3ef1449814469556c
George FunBook <gkurelic@gmail.com>
2019-08-24 15:21:24 -0500

    re-add User documentation

--- lib/Source/io/newgrounds/objects/User.hx
+++ lib/Source/io/newgrounds/objects/User.hx
@@ -2,9 +2,14 @@ package io.newgrounds.objects;
 
 typedef User = {
 	
+	/** The user's icon images. */
 	var icons    (default, null):UserIcons;
+	/** The user's numeric ID. */
 	var id       (default, null):Int;
+	/** The user's textual name. */ 
 	var name     (default, null):String;
+	/** Returns true if the user has a Newgrounds Supporter upgrade. */ 
 	var supporter(default, null):Bool;
+	/** The user's NG profile url. */
 	var url      (default, null):String;
 }
