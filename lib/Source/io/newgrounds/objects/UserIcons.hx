39285b2992c28db97bdbffce68ee9b5814f05276
George FunBook <gkurelic@gmail.com>
2019-08-24 15:22:46 -0500

    re-add UserIcon documentation

--- lib/Source/io/newgrounds/objects/UserIcons.hx
+++ lib/Source/io/newgrounds/objects/UserIcons.hx
@@ -2,7 +2,10 @@ package io.newgrounds.objects;
 
 typedef UserIcons = {
 	
+	/**The URL of the user's large icon. */
 	var large (default, null):String;
+	/** The URL of the user's medium icon. */
 	var medium(default, null):String;
+	/** The URL of the user's small icon. */
 	var small (default, null):String;
 }
